3d3475dad6e8d0d03f3db9e4d9e83554bffeb822
irwan <ryooriyagami@gmail.com>
2020-10-21 23:28:53 +0800

    tutorial php

---
+++ funct.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function sayHi($name, $age)
+        {
+            echo "Hello $name, you are $age. <br>";
+        }
+    
+        sayHi("irwan", 10);
+    
+        sayHi("umi", 30);
+    
+        sayHi("Amirah", 20);
+    ?>
+
+</body>
+</html>
